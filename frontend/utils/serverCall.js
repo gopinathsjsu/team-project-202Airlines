c7cc6f1bda5b962237ec100838a127320d4765f6
Anil Gubbala <anil.gubbala@sjsu.edu>
2021-10-15 15:17:15 -0700

    server call & setup code.

---
+++ frontend/utils/serverCall.js
@@ -0,0 +1,28 @@
+import Axios from 'axios';
+import { SERVER } from './consts';
+
+Axios.defaults.withCredentials = true;
+
+const get = (path, data) =>
+  Axios.get(SERVER.URL + path, { params: data })
+    .then((response) => response.data)
+    .catch((error) => {
+      if (error.response.data.err) {
+        throw error.response.data.err;
+      } else {
+        throw 'server side error';
+      }
+    });
+
+const post = (path, data) =>
+  Axios.post(SERVER.URL + path, data, { mode: 'cors' })
+    .then((response) => response.data)
+    .catch((error) => {
+      if (error.response.data.err) {
+        throw error.response.data.err;
+      } else {
+        throw 'server side error';
+      }
+    });
+
+export { get, post };
