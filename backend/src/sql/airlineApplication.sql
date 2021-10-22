82e898d27b2a71c05acdcc2be30b9205e126ff83
Himakshi <vhimakshi24@gmail.com>
2021-10-21 17:24:26 -0700

    Mock data for tables

--- backend/src/sql/airlineApplication.sql
+++ backend/src/sql/airlineApplication.sql
@@ -1,48 +1,60 @@
-drop database  if exists Airline;
-create database Airline;
-use Airline;
+drop database if exists Airlines;
+create database Airlines;
+use Airlines;
 
-create table airport (
-    airport_id int auto_increment,
-    airport_name varchar(250), 
-    airport_location varchar(250), 
-    primary key(airport_id)
+CREATE TABLE IF NOT EXISTS `Airlines`.`Airport` (
+  `airport_id` int NOT NULL AUTO_INCREMENT,
+  `airport_name` varchar(100) DEFAULT NULL,
+  `airport_location` varchar(100) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`airport_id`)
 );
 
-create table flight (
-    flight_id int auto_increment,
-    airport_id int not null,
-    flight_number int unique, 
-    flight_src varchar(250), 
-    flight_dst varchar(250),
-    flight_type varchar(250), 
-    start_time time,
-    end_time time, 
-    price decimal(15,2),
-    miles int,
-    primary key(flight_id),
-    foreign key (airport_id) references airport (airport_id) on update cascade on delete cascade
+CREATE TABLE IF NOT EXISTS `Airlines`.`Flight` (
+  `flight_id` int NOT NULL AUTO_INCREMENT,
+  `airport_id` int NOT NULL,
+  `flight_number` varchar(50) DEFAULT NULL,
+  `flight_src` varchar(100) DEFAULT NULL,
+  `flight_dst` varchar(100) DEFAULT NULL,
+  `flight_type` varchar(50) DEFAULT NULL,
+  `start_time` time DEFAULT NULL,
+  `end_time` time DEFAULT NULL,
+  `price` decimal(15,2) DEFAULT NULL,
+  `miles` int DEFAULT NULL,
+  PRIMARY KEY (`flight_id`),
+  UNIQUE KEY `flight_number` (`flight_number`),
+  KEY `airport_id` (`airport_id`),
+  CONSTRAINT `flight_ibfk_1` 
+    FOREIGN KEY (`airport_id`) 
+    REFERENCES `Airport` (`airport_id`) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE
 );
 
-create table customer (
-    customer_id int auto_increment,
-    customer_first_name varchar(250), 
-    customer_last_name varchar(250), 
-    address varchar(250), 
-    city varchar(250), 
-    state varchar(250), 
-    zip_code int, 
-    passportid varchar(250), 
-    gender varchar(250), 
-    password varchar(250),  
-    emailid varchar(250), 
-    role varchar(250), 
-    sec_ques varchar(250),
-    sec_ans varchar(250),  
-    mileage_plus_number varchar(250), 
-    total_miles int, 
-    primary key(customer_id)
+alter table Flight auto_increment = 100;
+
+CREATE TABLE IF NOT EXISTS `Airlines`.`Customer` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `customer_first_name` varchar(100) DEFAULT NULL,
+  `customer_last_name` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `zip_code` int DEFAULT NULL,
+  `passportid` varchar(100) DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `emailid` varchar(50) DEFAULT NULL,
+  `role` varchar(50) DEFAULT NULL,
+  `sec_ques` varchar(250) DEFAULT NULL,
+  `sec_ans` varchar(250) DEFAULT NULL,
+  `mileage_plus_number` varchar(50) DEFAULT NULL,
+  `total_miles` int DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
 );
+
+
 CREATE TABLE IF NOT EXISTS `Airlines`.`Booking` (
   `booking_id` INT NOT NULL AUTO_INCREMENT,
   `status` VARCHAR(45) NULL DEFAULT NULL,
@@ -60,16 +72,17 @@ CREATE TABLE IF NOT EXISTS `Airlines`.`Booking` (
   INDEX `cstmr_fk_idx` (`customer_id` ASC) VISIBLE,
   CONSTRAINT `cstmr_fk`
     FOREIGN KEY (`customer_id`)
-    REFERENCES `Airlines`.`customer` (`customer_id`)
+    REFERENCES `Airlines`.`Customer` (`customer_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `flight_fk`
     FOREIGN KEY (`flight_id`)
-    REFERENCES `Airlines`.`flight` (`flight_id`)
+    REFERENCES `Airlines`.`Flight` (`flight_id`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE);
+    ON UPDATE CASCADE
+);
  
- CREATE TABLE IF NOT EXISTS `Airlines`.`FlightSeat` (
+CREATE TABLE IF NOT EXISTS `Airlines`.`FlightSeat` (
   `flight_id` INT NOT NULL,
   `seat_no` VARCHAR(3) NULL DEFAULT NULL,
   `customer_id` INT NOT NULL,
@@ -86,21 +99,29 @@ CREATE TABLE IF NOT EXISTS `Airlines`.`Booking` (
     ON UPDATE CASCADE,
   CONSTRAINT `customer_id_fk`
     FOREIGN KEY (`customer_id`)
-    REFERENCES `Airlines`.`customer` (`customer_id`)
+    REFERENCES `Airlines`.`Customer` (`customer_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `flight_id_fk`
     FOREIGN KEY (`flight_id`)
-    REFERENCES `Airlines`.`flight` (`flight_id`)
+    REFERENCES `Airlines`.`Flight` (`flight_id`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE);
+    ON UPDATE CASCADE,
+CONSTRAINT `seat_no_fk`
+    FOREIGN KEY (`seat_no`)
+    REFERENCES `Airlines`.`Seat` (`seat_no`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
   
-  CREATE TABLE IF NOT EXISTS `Airlines`.`Seat` (
+CREATE TABLE IF NOT EXISTS `Airlines`.`Seat` (
   `seat_no` VARCHAR(3) NOT NULL,
   `class` VARCHAR(45) NULL DEFAULT NULL,
   `price` FLOAT NULL DEFAULT NULL,
-  PRIMARY KEY (`seat_no`));
-  CREATE TABLE IF NOT EXISTS `Airlines`.`Traveller` (
+  PRIMARY KEY (`seat_no`)
+);
+
+CREATE TABLE IF NOT EXISTS `Airlines`.`Traveller` (
   `name` VARCHAR(45) NOT NULL,
   `booking_id` INT NOT NULL,
   `gender` CHAR(1) NULL DEFAULT NULL,
@@ -111,4 +132,6 @@ CREATE TABLE IF NOT EXISTS `Airlines`.`Booking` (
     FOREIGN KEY (`booking_id`)
     REFERENCES `Airlines`.`Booking` (`booking_id`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE);
+    ON UPDATE CASCADE
+);
+
