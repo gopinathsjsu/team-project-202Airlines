71d2275b8b6f30c7c65045f1da0ec2a5e4a9f5fc
ISHA RAO <raoisha2193@gmail.com>
2021-11-07 18:33:38 -0800

    navigation from flight search to flight list screen,server connection for flight booking screen

--- backend/src/controllers/flightDetailsController.js
+++ backend/src/controllers/flightDetailsController.js
@@ -1,32 +1,32 @@
-
 const db = require("../utils/dbConnector");
 const SQL_FLIGHT = require("../Database/flight");
 
 const getFlightList = (req, res) => {
-    const {
-        book_with,
-        flying_from,
-        flying_to,
-        flight_date,
-        travellers,
-        flight_class
-    } = req.body;
-   
-    db.query(SQL_FLIGHT.GET_FLIGHT_LIST,[flight_date, flying_from, flying_to, travellers], (error, results, fields) => {
-        if (error) {
-            return console.error(error.message);
-        }
-        for(let i=0; i<results.length; i++){
-            results[i].miles = results[i].miles * travellers;
-            results[i].price = results[i].price * travellers;
-        }
-        res.send(results);
-    });
-}
+  const {
+    book_with,
+    flying_from,
+    flying_to,
+    flight_date,
+    travellers,
+    flight_class,
+  } = req.body;
 
-module.exports = { 
-    getFlightList
+  db.query(
+    SQL_FLIGHT.GET_FLIGHT_LIST,
+    [flight_date, flying_from, flying_to, travellers],
+    (error, results, fields) => {
+      if (error) {
+        return console.error(error.message);
+      }
+      for (let i = 0; i < results.length; i++) {
+        results[i].miles = results[i].miles * travellers;
+        results[i].price = results[i].price * travellers;
+      }
+      res.send(results);
+    }
+  );
 };
 
-
-
+module.exports = {
+  getFlightList,
+};
--- frontend/src/views/FlightList.js
+++ frontend/src/views/FlightList.js
@@ -1,94 +1,100 @@
-import React from 'react';
-import Axios from "axios";
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import moment from 'moment';
+import { useParams, useHistory } from 'react-router-dom';
 import flightLogo from '../images/flightlogo.png';
-import { useEffect, useState } from 'react';
-import { useParams, useHistory } from "react-router-dom";
-import backendServer from "../webConfig";
+import backendServer from '../webConfig';
 
 let rows = [];
 function FlightList() {
-    Axios.defaults.withCredentials = true;
-    const [loading, setLoading] = useState(true);
+  Axios.defaults.withCredentials = true;
+  const [loading, setLoading] = useState(true);
 
-    let { details } = useParams();
-    let flightSearchDetails = JSON.parse(decodeURIComponent(details));
-    let displayDate = moment(flightSearchDetails.flight_date).format('MMMM Do, YYYY');;
+  const { details } = useParams();
+  const flightSearchDetails = JSON.parse(decodeURIComponent(details));
+  const displayDate = moment(flightSearchDetails.flight_date).format('MMMM Do, YYYY');
 
-    useEffect(() => {
-        console.log(flightSearchDetails);
-        Axios.post(`${backendServer}/flightList`, flightSearchDetails).then(function (res) {
-            console.log(res);
-            rows = res.data;
-            setLoading(false);
-        });
-    }, []);
+  useEffect(() => {
+    console.log(flightSearchDetails);
+    Axios.post(`${backendServer}/flightList`, flightSearchDetails).then((res) => {
+      console.log(res);
+      rows = res.data;
+      setLoading(false);
+    });
+  }, []);
 
-        return (
-           
-            <div className="container">
-                 <div className="row">
-                    <div className="col-3"></div>
-                    <div className="col-6 row display-text">
-                        <div className="col-4 text-center" >{flightSearchDetails.flying_from}</div>
-                        <div className="col-4 text-center">
-                            <img src={flightLogo} alt=" " width="100" height="100"></img>
-                        </div>
-                        <div className="col-4 text-center">{flightSearchDetails.flying_to}</div>
-                    </div>
-                    <div className="col-3"></div>
-                </div>
-                <br/>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-3" />
+        <div className="col-6 row display-text">
+          <div className="col-4 text-center">{flightSearchDetails.flying_from}</div>
+          <div className="col-4 text-center">
+            <img src={flightLogo} alt=" " width="100" height="100" />
+          </div>
+          <div className="col-4 text-center">{flightSearchDetails.flying_to}</div>
+        </div>
+        <div className="col-3" />
+      </div>
+      <br />
 
-                <div className="row">
-                    <h2 className="col text-center">{displayDate}</h2>
-                </div>
+      <div className="row">
+        <h2 className="col text-center">{displayDate}</h2>
+      </div>
 
-                <br/>
-                <br/>
+      <br />
+      <br />
 
-                
-                {
-                (loading)?( 
-                    <div>Checking if flights are available for the selected....</div>
-                ):(
-                    
-                    
-                    (rows.length > 0)?(
-                    
-                    <div>
-                    <div className="row row-cols-6">
-                        <div className="col"><b>Flight Number</b></div>
-                        <div className="col"><b>Start Time</b></div>
-                        <div className="col"><b>End Time</b></div>
-                        <div className="col"><b>Duration</b></div>
-                        <div className="col"><b>{flightSearchDetails.book_with}</b></div>
-
-                    </div>
-                     <br/>
-
-                    {rows.map(res =>
-                        <div className="row row-cols-6" key={res.flight_number}>
-                            <div className="col">{res.flight_number}</div>
-                            <div className="col">{res.start_time}</div>
-                            <div className="col">{res.end_time}</div>
-                            <div className="col">{(moment.duration(moment(res.end_time,"HH:mm:ss").diff(moment(res.start_time,"HH:mm:ss")))).asHours()} hours</div>
-                            <div className="col">{(flightSearchDetails.book_with==="Money")?"$":""}{(flightSearchDetails.book_with==="Money")?res.price:res.miles}</div>
-                            <button className="btn btn-default" type="button">Select</button>
-                        </div>
-                    )}
-                    </div>
-                    ):(
-                        <h2 className="text-danger">No Flights Are Available for the Selected dates</h2>
-                    )
-                    
-                )
-
-            }
-                
+      {loading ? (
+        <div>Checking if flights are available for the selected....</div>
+      ) : rows.length > 0 ? (
+        <div>
+          <div className="row row-cols-6">
+            <div className="col">
+              <b>Flight Number</b>
             </div>
-            
-        );
-    
+            <div className="col">
+              <b>Start Time</b>
+            </div>
+            <div className="col">
+              <b>End Time</b>
+            </div>
+            <div className="col">
+              <b>Duration</b>
+            </div>
+            <div className="col">
+              <b>{flightSearchDetails.book_with}</b>
+            </div>
+          </div>
+          <br />
+
+          {rows.map((res) => (
+            <div className="row row-cols-6" key={res.flight_number}>
+              <div className="col">{res.flight_number}</div>
+              <div className="col">{res.start_time}</div>
+              <div className="col">{res.end_time}</div>
+              <div className="col">
+                {moment
+                  .duration(
+                    moment(res.end_time, 'HH:mm:ss').diff(moment(res.start_time, 'HH:mm:ss'))
+                  )
+                  .asHours()}{' '}
+                hours
+              </div>
+              <div className="col">
+                {flightSearchDetails.book_with === 'Money' ? '$' : ''}
+                {flightSearchDetails.book_with === 'Money' ? res.price : res.miles}
+              </div>
+              <button className="btn btn-default" type="button">
+                Select
+              </button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h2 className="text-danger">No Flights Are Available for the Selected dates</h2>
+      )}
+    </div>
+  );
 }
 export default FlightList;
