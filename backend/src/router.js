8eec07478bc17b76eb152a7c810d156dfb4c2a0f
ISHA RAO <raoisha2193@gmail.com>
2021-11-07 18:33:38 -0800

    navigation from flight search to flight list screen,server connection for flight booking screen

--- backend/src/router.js
+++ backend/src/router.js
@@ -11,7 +11,6 @@ const { getFlightList } = require("./controllers/flightDetailsController");
 
 const router = express.Router();
 
-// demo
 router.route("/demoCall").post(demoCall);
 router.route("/signin").post(signin);
 router.route("/getBookingHistory").get(getBookingHistory);
